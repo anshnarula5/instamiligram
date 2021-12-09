44c09d6587fa32b64c16846391cf61cccf2dfffe
Ansh Narula <2019210@iiitdmj.ac.in>
2021-12-10 01:02:33 +0530

    deploy

--- backend/server.js
+++ backend/server.js
@@ -34,8 +34,8 @@ app.use(errorHandler);
 
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")))
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html")))
+  app.use(express.static(path.join(__dirname, "/client/build")))
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname,  "client", "build", "index.html")))
 }
 
 
