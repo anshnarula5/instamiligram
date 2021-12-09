6087876900e7c44a1cbb55febb2048f71a0f5c69
Ansh Narula <2019210@iiitdmj.ac.in>
2021-12-10 00:49:18 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,13 @@ app.use(notFound);
 
 app.use(errorHandler);
 
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")))
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "..", "client", "build", "index.html")))
+}
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Running on ${PORT}`));
